a96511ba07e7948af6c611091a853ce31c42f11c
serg170380 <serg170380@yandex.ru>
2022-02-04 20:31:35 +0300

    ДЗ №7

---
+++ HW7/Map.java
@@ -0,0 +1,12 @@
+package HW7;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Map extends JPanel {
+
+    Map() {
+        setBackground(new Color(150,140,250));
+    }
+
+}
---
+++ HW7/Starter.java
@@ -0,0 +1,10 @@
+package HW7;
+
+public class Starter {
+
+    public static void main(String[] args) {
+        HomeWork7 homeWork7 = new HomeWork7();
+
+
+    }
+}
